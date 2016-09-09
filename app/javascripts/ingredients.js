17827688e00b351cd7455dd2a409f8d0697a9dfe
pavenuto <pavenuto@gmail.com>
2016-09-09 13:35:39 -0400

    tweaks

---
+++ app/javascripts/ingredients.js
@@ -0,0 +1,39 @@
+export let rices = [
+  'White Rice',
+  'Brown Rice'
+];
+
+export let proteins = [
+  'Beef',
+  'Chicken',
+  'Spicy Chicken',
+  'Spicy Pork',
+  'Pork Belly',
+  'Tofu'
+];
+
+export let toppings = [
+  'Lettuce',
+  'Corn',
+  'Bean Sprouts',
+  'Pickled Radish',
+  'Pickled Cucumber',
+  'Roasted Carrots',
+  'Black Beans',
+  'Edamame',
+  'Kim-Chee',
+  'Roasted Onion',
+  'Cheese',
+  'Seaweed'
+];
+
+export let sauces = [
+  'Ginger Sauce (Mild)',
+  'Teriyaki Sauce (Mild)',
+  'Creamy Chili Sauce (Mild)',
+  'Bull Sauce (Mild)',
+  'Spicy Teriyaki Sauce (Hot)',
+  'Korean Taco Sauce (Hot)',
+  'Z-Kick Sauce (Very Hot)',
+  'Angry Bull Sauce (Very Hot)'
+];
